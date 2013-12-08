c5f029f24419f4edc5046998030e03769b2f7eff
simon <simon@192.168.1.114>
2013-12-08 20:07:11 +0100

    Added foreign keys.

--- master/db-build/src/main/resources/db/migration/V1.0.015__create_table_person.sql
+++ master/db-build/src/main/resources/db/migration/V1.0.015__create_table_person.sql
@@ -1,7 +1,10 @@
 create table PERSON (
-    ID int(9) not null AUTO_INCREMENT PRIMARY KEY,
+    ID            int(9)          not null AUTO_INCREMENT PRIMARY KEY,
     NAME          varchar(100)    not null,
     FIRSTNAME     varchar(100),
     ADDRESS_ID    int(9)          not null,
-    CONTACT_ID    int(9)
+    CONTACT_ID    int(9)          not null
 );
+
+alter table PERSON add constraint FK_PERSON_ADDRESS foreign key (ADDRESS_ID) REFERENCES ADDRESS(ID);
+alter table PERSON add constraint FK_PERSON_CONTACT foreign key (CONTACT_ID) REFERENCES CONTACT(ID);
